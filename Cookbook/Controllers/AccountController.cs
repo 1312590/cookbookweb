1236ca6d72719ff28c8a058a532015156aa82523
Ronswills <npt1995.khtn@gmail.com>
2016-12-20 15:17:23 +0700

    Fuck

--- Cookbook/Controllers/AccountController.cs
+++ Cookbook/Controllers/AccountController.cs
@@ -41,7 +41,7 @@ namespace Cookbook.Controllers
 
             try
             {
-                var json = client.DownloadString(String.Format("http://cookbookapi.apphb.com/api/NguoiDung/Get/{0}", id));
+                var json = client.DownloadString(String.Format("http://cookbookapi-9.apphb.com/api/NguoiDung/Get/{0}", id));
                 var serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
                 dynamic model = serializer.Deserialize<dynamic>(json);
 
@@ -53,7 +53,7 @@ namespace Cookbook.Controllers
             {
                 Session["IsNewUser"] = Boolean.TrueString;
 
-                Account account = new Account("hl6ei8egh", "132439745671385", "f7bunvOJ6Fg6Bqynodcx5TRQfaY");
+                Account account = new Account("ds7oqgjml", "279246699794423", "qsmFs621AzHPDsbntISJ_6_bX14");
                 Cloudinary cloudinary = new Cloudinary(account);
 
                 var uploadParams = new ImageUploadParams()
